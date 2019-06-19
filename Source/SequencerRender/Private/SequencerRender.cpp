3c48be7467b0435329fa5199920be8448203897b
ilgarlunin <shermand07@mail.ru>
2019-06-19 14:05:10 +0300

    support dumb players

--- Source/SequencerRender/Private/SequencerRender.cpp
+++ Source/SequencerRender/Private/SequencerRender.cpp
@@ -196,7 +196,7 @@ void FSequencerRenderModule::PluginButtonClicked()
                             FPlatformProcess::CreatePipe(PipeRead, PipeWrite);
 
                             int32 ReturnCode = -1;
-                            FString args = FString::Printf(TEXT("-start_number %d -i image.%%04d.png -vcodec libx264 -crf 10 -b 10M -y -filter:v fps=fps=%f %s"), startFrame, captureFps, *fileName);
+                            FString args = FString::Printf(TEXT("-start_number %d -i image.%%04d.png -vcodec libx264 -pix_fmt yuv420p -crf 10 -b 10M -y -filter:v fps=fps=%f %s"), startFrame, captureFps, *fileName);
                             FString output;
                             FProcHandle pHandle = FPlatformProcess::CreateProc(*ffmpegExe, *args, false, true, true, nullptr, 0, *captureOutputDirectory, PipeWrite);
                             int32 retCode = 0;
